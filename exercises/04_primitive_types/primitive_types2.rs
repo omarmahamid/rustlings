8dedec76f58ae405918e6613f8662359dbfb8f14
omar.mahamid <omar.taiseerr@gmail.com>
2024-06-19 01:15:08 +0300

    add rust

--- exercises/04_primitive_types/primitive_types2.rs
+++ exercises/04_primitive_types/primitive_types2.rs
@@ -1,3 +1,4 @@
+
 // primitive_types2.rs
 //
 // Fill in the rest of the line that has code missing! No hints, there's no
@@ -19,7 +20,7 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    let // Finish this line like the example! What's your favorite character?
+    let your_character = 'D';// Finish this line like the example! What's your favorite character?
     // Try a letter, try a number, try a special character, try a character
     // from a different language than your own, try an emoji!
     if your_character.is_alphabetic() {
